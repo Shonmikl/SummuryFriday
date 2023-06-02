f29c452714f772b6a7b49476343107e7c643c589
shonmikl <mikhailegorov676@gmail.com>
2023-06-02 12:46:07 +0200

    commit 2

--- src/main/java/org/example/_2023_06_02/Test1.java
+++ src/main/java/org/example/_2023_06_02/Test1.java
@@ -1,4 +1,10 @@
 package org.example._2023_06_02;
 
-public class Test1 {
+public class Test1 extends Object {
+    //Инкапсуляция - сокрытие реализации
+    //Наследование - вступление в отношения с другим классом ради использования его свойств
+    //Полиморфизм -
+    public static void main(String[] args) {
+
+    }
 }
