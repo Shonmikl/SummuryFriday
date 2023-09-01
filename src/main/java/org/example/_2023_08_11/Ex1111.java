e6417f5304d18dda07941b4d6c2ed2de8dd73c90
shonmikl <mikhailegorov676@gmail.com>
2023-09-01 09:39:58 +0200

    stream()

---
+++ src/main/java/org/example/_2023_08_11/Ex1111.java
@@ -0,0 +1,13 @@
+package org.example._2023_08_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1111 {
+    public static void main(String[] args) {
+        /**
+         * List
+         */
+        List<Integer> list = new ArrayList<>();
+    }
+}
