8ecb544be27119d972c268eb49ffd6905428e9ba
shonmikl <mikhailegorov676@gmail.com>
2023-09-22 10:17:21 +0200

    Parameter

---
+++ src/main/java/org/example/_2023_09_22/ex/TestGame.java
@@ -0,0 +1,9 @@
+package org.example._2023_09_22.ex;
+
+import com.github.javafaker.Faker;
+
+public class TestGame {
+    public static void main(String[] args) {
+        Faker faker = new Faker();
+    }
+}
